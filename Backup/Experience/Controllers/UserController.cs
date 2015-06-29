8ac7e6d3e67e9bea5e9225af5bfa72e2983791b6
alexandr.amostap <alexandr.amostap@gmail.com>
2015-06-29 23:23:40 +0300

    first

---
+++ Backup/Experience/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Experience.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        UserLogic logic = new UserLogic();
+
+        public ActionResult Index()
+        {
+            ViewBag.NumberOfUsers = logic.GetNumberOfUsers();
+            return View(logic.GetUsers());
+        }
+
+        public ActionResult Customer()
+        {
+            ViewBag.NumberOfUsers = logic.GetNumberOfCustomers();
+            return View(logic.GetCustomers());
+        }
+
+        public ActionResult Perfomer()
+        {
+            ViewBag.NumberOfUsers = logic.GetNumberOfPerformers();
+            return View(logic.GetPerformers());
+        }
+
+    }
+}
---
+++ Backup/Experience/Models/AccountModels.cs
@@ -0,0 +1,120 @@
+﻿using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace Experience.Models
+{
+    //public class UsersContext : DbContext
+    //{
+    //    public UsersContext()
+    //        : base("DefaultConnection")
+    //    {
+    //    }
+
+    //    public DbSet<UserProfile> UserProfiles { get; set; }
+    //}
+
+    //[Table("UserProfile")]
+    //public class UserProfile
+    //{
+    //    [Key]
+    //    [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+    //    public int UserId { get; set; }
+    //    public string UserName { get; set; }
+    //}
+
+    public class RegisterExternalLoginModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        public string ExternalLoginData { get; set; }
+    }
+
+    public class LocalPasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        //public string Surname { get; set; }
+        //public string PhoneNumber { get; set; }
+        //public string Email { get; set; }
+        //public virtual UserType Type { get; set; }
+
+        [Required]
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+        [Required]
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Готов ли трудиться?")]
+        public bool isReady { get; set; }
+        //[Required]
+        //[Display(Name = "Type")]
+        //public UserType Type { get; set; }
+        //public string Type { get; set; }
+    }
+
+    public class ExternalLogin
+    {
+        public string Provider { get; set; }
+        public string ProviderDisplayName { get; set; }
+        public string ProviderUserId { get; set; }
+    }
+}
